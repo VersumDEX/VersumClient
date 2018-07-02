36de502c76fb75e2e4b9309228b785ae9d07ce46
VersumDEX <support@versum.io>
2018-07-02 12:45:52 +0200

    update

--- src/conf.py
+++ src/conf.py
@@ -1,3 +1,3 @@
 chainname = "mainnet/testnet"#currently on "testchain" (our first testnet)
 chaindata = "any node address connected to the network"#e.g. testchain@175.215.136.219:6804
-gateway_ip = "gateway-node address"#not available to the public
+gateway_ip = "gateway-node address"#not available to the public at the moment
